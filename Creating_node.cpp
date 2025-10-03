99d9f5efb4d650f33057da54a6ea722a431f8f1b
Dev <devkdarji@gmail.com>
2025-10-03 22:18:40 +0530

    Create Creating_node.cpp

---
+++ Creating_node.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+    public : 
+    int val;
+    Node *next;
+
+    // Constructor to initialize node
+     Node(int data){
+        val = data;
+        next = NULL;
+    }
+   
+};
+
+int main(){
+    Node*n = new Node(50);
+    // Syntax of linked list 
+    cout<<n->val<<" "<<n->next<<endl;
+    return 0;
+}
+
+/*
+OUUTPUT:
+50 0
+*/
